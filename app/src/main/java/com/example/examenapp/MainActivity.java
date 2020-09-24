5c40b7fe44771d85cf8fc52c7ceded033812ae24
Diego <loherdiego@gmail.com>
2020-09-24 18:18:14 -0500

    Activities creados

--- app/src/main/java/com/example/examenapp/MainActivity.java
+++ app/src/main/java/com/example/examenapp/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.examenapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=findViewById(R.id.btnContinuar);
+        //Intent intent=new Intent(MainActivity.this,MainActivity2);
+        //startActivity(intent);
     }
 }
